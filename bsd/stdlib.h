ca57ddf81b825252bd6d17692518f075ea113b96
John Marino <draco@marino.st>
2017-11-29 13:43:14 -0600

    Rename arc4random functions to avoid clash with static ssl

--- bsd/stdlib.h
+++ bsd/stdlib.h
@@ -9,11 +9,17 @@ void *reallocf(void *ptr, size_t size);
 void *reallocarray(void *ptr, size_t nmemb, size_t size);
 const char *getprogname();
 
-uint32_t arc4random(void);
-void arc4random_stir(void);
-void arc4random_addrandom(unsigned char *dat, int datlen);
-void arc4random_buf(void *_buf, size_t n);
-uint32_t arc4random_uniform(uint32_t upper_bound);
+#define	arc4random		sol_arc4random
+#define	arc4random_addrandom	sol_arc4random_addrandom
+#define	arc4random_buf		sol_arc4random_buf
+#define	arc4random_stir		sol_arc4random_stir
+#define	arc4random_uniform	sol_arc4random_uniform
+
+uint32_t	arc4random(void);
+void		arc4random_stir(void);
+void		arc4random_addrandom(unsigned char *dat, int datlen);
+void		arc4random_buf(void *_buf, size_t n);
+uint32_t	arc4random_uniform(uint32_t upper_bound);
 
 long long strtonum(const char *nptr, long long minval, long long maxval,
                    const char **errstr);
