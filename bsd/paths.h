354d353848f014e3b1040b34a803b544f0d8b209
John Marino <draco@marino.st>
2017-12-17 00:34:35 -0600

    solaris: set modern bourne shell path

--- bsd/paths.h
+++ bsd/paths.h
@@ -1,7 +1,7 @@
 #ifndef LIBBSD_PATHS_H
 #define LIBBSD_PATHS_H
 
-#define	_PATH_BSHELL	"/bin/sh"
+#define	_PATH_BSHELL	"/usr/xpg4/bin/sh"
 #define	_PATH_TTY	"/dev/tty"
 #define	_PATH_DEVNULL	"/dev/null"
 #define _PATH_LIBC64	"/lib/64/libc.so.1"
