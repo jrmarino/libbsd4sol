8fd37d93b3cc296b072965758cb349c1f8a1412c
John Marino <draco@marino.st>
2018-01-31 15:48:26 -0600

    add strnlen function

--- bsd/string.h
+++ bsd/string.h
@@ -9,6 +9,7 @@ char	*strsep(char **, const char *);
 char	*strndup(const char *str, size_t n);
 char	*strcasestr(const char *, const char *);
 void	strmode(mode_t mode, char *str);
+size_t	strnlen(const char *, size_t);
 
 void	explicit_bzero(void *buf, size_t len);
 #endif
